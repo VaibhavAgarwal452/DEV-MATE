b2242803a3146e3c1c5ce2530254ba8ca460dab9
Vaibhav Agarwal <vaibhavagarwal78@gmail.com>
2021-01-13 23:16:00 +0530

    Prepared for Deployment

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
 const connectDB = require('./config/db');
 const app = express();
+const path = require('path');
 
 connectDB();
 
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => {
-  res.send('API RUNNING');
-});
 
 // Define Routes
 
@@ -16,6 +13,14 @@ app.use('/api/users', require('./routes/api/users'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profiles', require('./routes/api/profiles'));
 app.use('/api/auth', require('./routes/api/auth'));
+
+if (process.env.NODE_ENV == 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 const PORT = process.env.PORT || 5001;
 
